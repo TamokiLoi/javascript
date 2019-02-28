c1ce392ad49e834b5e63edfd0dc6cfc967e5cf2c
loinlt <loinguyenlamthanh@gmail.com>
2019-02-28 15:51:51 +0700

    update effect-interactive-facebook

--- JS-Fedu/effect-interactive-facebook/1.js
+++ JS-Fedu/effect-interactive-facebook/1.js
@@ -1,27 +1,31 @@
-// basic
+/* begin basic */
 // document.addEventListener("DOMContentLoaded", function () {
 // 	// declare variable
 // 	var triangles = document.getElementsByClassName('triangle-icon');
 // 	var triangle = triangles[0];
 // 	var listContents = document.getElementsByClassName('list-content');
 // 	var listContent = listContents[0];
-
 // 	// used function onclick and toggle class for triangle transform color
 // 	triangle.onclick = function() {
-// 		// used style
-// 		// this.style.color = 'white';
-
+// 		used style
+// 		this.style.color = 'white';
 // 		this.classList.toggle('triangle-white');
 // 		listContent.classList.toggle('list-content-show');
 // 	};
 // }, false);
+/* end basic */
 
-// advance
-document.addEventListener("DOMContentLoaded", function() {
-    var btnEffects = document.getElementsByClassName('btn-effect');
-    for (var i = 0; i < btnEffects.length; i++) {
-        btnEffects[i].onclick = function() {
-            console.log(this.getAttribute('data-password'));
-        }
-    }
-}, false);
+/* begin advance */
+// document.addEventListener("DOMContentLoaded", function() {
+//     var btnEffects = document.getElementsByClassName('btn-effect');
+//     for (var i = 0; i < btnEffects.length; i++) {
+//         btnEffects[i].onclick = function() {
+//             console.log(this.getAttribute('data-password'));
+//             console.log(this.getAttribute('data-info'));
+//         }
+//     }
+// }, false);
+/* end advance */
+
+/* effect interactive facebook */
+
