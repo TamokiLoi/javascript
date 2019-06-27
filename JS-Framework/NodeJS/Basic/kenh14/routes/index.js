1dcd78be087e5d930811dbaa85dc99c5f9185d65
loinlt <loinguyenlamthanh@gmail.com>
2019-06-27 17:08:50 +0700

    demo layout html by ejs

--- JS-Framework/NodeJS/Basic/kenh14/routes/index.js
+++ JS-Framework/NodeJS/Basic/kenh14/routes/index.js
@@ -3,17 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express'});
 });
 
-/* GET news page. */
-router.get('/news', function(req, res, next) {
-  res.render('new', { title: 'News' });
+/* GET about page. */
+router.get('/about.html', function(req, res, next) {
+  res.render('about', { title: 'About'});
 });
 
-/* GET products page. */
-router.get('/products', function(req, res, next) {
-  res.render('product', { title: 'Products', content: 'This is page product' });
+/* GET post page. */
+router.get('/post.html', function(req, res, next) {
+  res.render('post', { title: 'Sample Post'});
+});
+
+/* GET contact page. */
+router.get('/contact.html', function(req, res, next) {
+  res.render('contact', { title: 'Contact'});
 });
 
 module.exports = router;
