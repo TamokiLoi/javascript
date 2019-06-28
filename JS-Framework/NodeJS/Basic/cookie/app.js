b68a63368c053241d4d3506d46662ebf91ed3ee4
loinlt <loinguyenlamthanh@gmail.com>
2019-06-28 14:46:17 +0700

    update session demo

--- JS-Framework/NodeJS/Basic/cookie/app.js
+++ JS-Framework/NodeJS/Basic/cookie/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var session = require('express-session');
 
 var app = express();
 
@@ -17,6 +18,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+  secret: 'T@Moki110292',
+  resave: false,
+  saveUninitialized: true,
+  // cookie: { secure: true }
+}))
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- JS-Framework/NodeJS/Basic/cookie/routes/index.js
+++ JS-Framework/NodeJS/Basic/cookie/routes/index.js
@@ -6,4 +6,40 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET send number page. */
+router.get('/send/:number', function(req, res, next) {
+  res.cookie('phoneNumber', req.params.number, {maxAge:90000});
+  res.send('Phone number send is: ' + req.params.number);
+});
+
+/* GET get number page. */
+router.get('/get', function(req, res, next) {
+  res.send('Phone number get is: ' + req.cookies.phoneNumber);
+});
+
+/* GET clear number page. */
+router.get('/clear', function(req, res, next) {
+  res.clearCookie('phoneNumber');
+  res.send('Phone Number is deleted');
+});
+
+/* GET create session  page. */
+router.get('/create-session', function(req, res, next) {
+  req.session.password = '123456';
+  res.send('Created session');
+});
+
+/* GET get session  page. */
+router.get('/get-session', function(req, res, next) {
+  res.send('Session is: ' + req.session.password);
+});
+
+/* GET clear session  page. */
+router.get('/clear-session', function(req, res, next) {
+  req.session.destroy(function(err) {
+    console.log(err)
+  });
+  res.send('Session is deleted:');
+});
+
 module.exports = router;
