1cd7d2fc82aa0260f03169f7840a8f61044bf70f
TamokiLoi <loinguyenlamthanh@gmail.com>
2020-03-24 19:43:13 +0700

    update folder vuejs project learn-words

---
+++ JS-Framework/VueJS/Basic/learn-words/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <app-card card-words="cat">
+            <h2 slot="title">{{ cardTitle }}</h2>
+            <p>White cat is so noob!</p>
+          </app-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "./components/Card";
+
+export default {
+  data() {
+    return {
+      cardTitle: 'White cat'
+    }
+  },
+  components: {
+    appCard: Card
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ JS-Framework/VueJS/Basic/learn-words/src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <!-- <h2>{{ cardWords }}</h2>
+        <p>This is my car!</p> -->
+        <div class="title">
+            <slot name="title"></slot>
+            <span style="color: #ccc;"><slot name="subTitle">The Subtitle</slot></span>
+        </div>
+
+        <div class="content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['cardWords']
+}
+</script>
+
+<style scoped>
+    div {
+        width: 100%;
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px rgba(0, 0, 0, .6);
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center;
+    }
+    h2 {
+        color: red
+    }
+
+    .title {
+        font-style: italic;
+    }
+</style>
